0cb06c83b18835145634afb0e7adfb08cbaf9e01
nickteojs <finalmoweyy@gmail.com>
2025-01-18 16:16:23 +0800

    Restructure exercise data, add mins and secs to rest input, fix bug when saving empty set

--- main.js
+++ main.js
@@ -8,69 +8,111 @@ const backButton = document.querySelector('#back-button');
 const saveButton = document.querySelector('#save');
 const pastSetsContainer = document.querySelector("#past-sets");
 
-const weightInput = document.querySelector(".weightInput");
-const repsInput = document.querySelector(".repsInput");
-const restInput = document.querySelector(".restInput");
+const exerciseForm = document.querySelector('#setInput');
+const weightInput = document.querySelector("#weightInput");
+const repsInput = document.querySelector("#repsInput");
+const restMinutes = document.querySelector("#restMinutes");
+const restSeconds = document.querySelector("#restSeconds");
 
 const currentSetsContainer = document.querySelector('#current-sets');
 
-const createCurrentSet = (currentSetsArray) => {
-    if (currentSetsArray.target.currentArray.length === 0) {
-        const date = new Date();
-        const dateString = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-        const dateAndId = {
-            date: dateString,
-            id: `${date.getHours()}${date.getMinutes()}${date.getSeconds()}-${date.getDate()}${date.getMonth() + 1}${date.getFullYear()}-${currentSetsArray.target.name}`
-        }
-        currentSetsArray.target.currentArray.push(dateAndId);
+const createChart = (exerciseName) => {
+    // 
+}
+
+var options = {
+    chart: {
+      type: 'line'
+    },
+    series: [{
+      name: 'sales',
+      data: [30,40,35,50,49,60,70,91,125]
+    }],
+    xaxis: {
+      categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
     }
-    currentSetsArray.target.currentArray.push({
+  }
+  
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  
+  chart.render();
+
+const formatRestTime = (minutes, seconds) => {
+    console.log(minutes, seconds);
+    let finalString;
+    if (minutes === 0 && seconds === 0) {
+        finalString = '0 rest';
+    } else if (minutes !== 0 && seconds === 0) {
+        finalString = `${minutes}m rest`;
+    } else if (minutes === 0 && seconds !== 0) {
+        finalString = `${seconds}s rest`;
+    } else {
+        finalString = `${minutes}m ${seconds}s rest`
+    }
+    return finalString;
+}
+
+const createCurrentSet = (e) => {
+    e.preventDefault();
+    let setGroup = e.target.setGroup;
+    const date = new Date();
+    // If brand new set, create date and ID for setGroup
+    if (Object.keys(setGroup).length === 0) {
+        console.log("Brand new set..")
+        setGroup.id = `${date.getHours()}${date.getMinutes()}${date.getSeconds()}-${date.getDate()}${date.getMonth() + 1}${date.getFullYear()}-${e.target.name}`
+        setGroup.date = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+        setGroup.sets = []
+    } 
+    setGroup.sets.push({
         'weight': +weightInput.value,
         'reps': +repsInput.value,
-        'rest': +restInput.value
-    })
+        'restMinutes': +restMinutes.value,
+        'restSeconds': +restSeconds.value
+    });
+
+    // Render current sets
     const newSet = document.createElement('div');
     const setWeight = document.createElement('div');
     const setReps = document.createElement('div');
     const setRest = document.createElement('div');
     newSet.className = 'set';
-    setWeight.textContent = currentSetsArray.target.currentArray.length === 0 ? '1. ' : `${currentSetsArray.target.currentArray.length - 1}. ` + weightInput.value + 'kg';
-    setReps.textContent = repsInput.value + 'reps';
-    setRest.textContent = restInput.value === '' ? '0 rest' : restInput.value + 'rest';
+    setWeight.textContent = `${setGroup.sets.length}. ` + weightInput.value + 'kg';
+    setReps.textContent = repsInput.value + ' reps';
+    setRest.textContent = formatRestTime(+restMinutes.value, +restSeconds.value);
     newSet.appendChild(setWeight);
     newSet.appendChild(setReps);
     newSet.appendChild(setRest);
     currentSetsContainer.appendChild(newSet);
     weightInput.value = '';
     repsInput.value = '';
-    restInput.value = '';
+    restMinutes.value = '';
+    restSeconds.value = '';
 }
 
 const saveSetToStorage = (event) => {
-    const { name, exercise, currentSets } = event.target;
+    const { name, exercise, setGroup } = event.target;
     console.log(name);
     console.log(exercise);
-    console.log(currentSets);
-    // If exercise from localStorage doesn't exist
-    if (!exercise) {
-        localStorage.setItem(name, JSON.stringify([currentSets]));
+    console.log(setGroup);
+    // If there are no past sets
+    if (JSON.parse(exercise).length === 0) {
+        localStorage.setItem(name, JSON.stringify([setGroup]));
     } else {
-        // If exercise exists
         // Fetch exercise
         const exerciseToUpdate = JSON.parse(localStorage.getItem(name));
         // console.log(exerciseToUpdate);
-        exerciseToUpdate.unshift(currentSets);
+        exerciseToUpdate.unshift(setGroup);
         localStorage.setItem(name, JSON.stringify(exerciseToUpdate));
     }
     renderPastSets(name);
-    currentSets.length = 0;
+    Object.keys(setGroup).forEach(set => delete setGroup[set]);
     currentSetsContainer.innerHTML = '';
 }
 
 const deleteSetFromStorage = e => {
-    const setToDelete = JSON.parse(localStorage.getItem(e.target.name));
+    const exerciseToDeleteFrom = JSON.parse(localStorage.getItem(e.target.name));
     const setId = e.target.id;
-    const updatedPastSets = setToDelete.filter(set => set[0].id !== setId);
+    const updatedPastSets = exerciseToDeleteFrom.filter(set => set.id !== setId);
     if (updatedPastSets.length === 0) {
         localStorage.setItem(e.target.name, JSON.stringify([]));
     } else {
@@ -84,37 +126,36 @@ const renderPastSets = (name) => {
     console.log("Past sets found, adding to past sets");
     const pastSets = JSON.parse(localStorage.getItem(name));
     console.log(pastSets)
-    pastSets.forEach((set, setIndex) => {
+    pastSets.forEach(set => {
         const setToAppend = document.createElement('div');
         setToAppend.className = 'past-set-item';
-        set.forEach((setItem, idx) => {
-            if (idx === 0) {
-                const dateElement = document.createElement('div');
-                dateElement.textContent = setItem.date;
-                dateElement.className = 'set-date';
-                setToAppend.appendChild(dateElement);
-            } else {
-                const setInformationDiv = document.createElement('div');
-                setInformationDiv.className = 'set-information';
-                const setWeight = document.createElement('div');
-                setWeight.textContent = setItem.weight + 'kg';
-                const setReps = document.createElement('div');
-                setReps.textContent = setItem.reps + 'reps';
-                const setRest = document.createElement('div');
-                setRest.textContent = setItem.rest + 'rest';
-                const deleteButton = document.createElement('div');
-                deleteButton.className = 'delete-set';
-                deleteButton.textContent = 'x';
-                deleteButton.id = set[0].id;
-                deleteButton.addEventListener('click', deleteSetFromStorage);
-                deleteButton.name = name;
-                setInformationDiv.appendChild(setWeight);
-                setInformationDiv.appendChild(setReps);
-                setInformationDiv.appendChild(setRest);
-                setToAppend.appendChild(setInformationDiv);
-                setToAppend.appendChild(deleteButton);
-            }
+        const dateElement = document.createElement('div');
+        dateElement.textContent = set.date;
+        dateElement.className = 'set-date';
+        setToAppend.appendChild(dateElement);
+        
+        // Creating subsets within a set
+        set.sets.forEach(subset => {
+            const setInformationDiv = document.createElement('div');
+            setInformationDiv.className = 'set-information';
+            const setWeight = document.createElement('div');
+            setWeight.textContent = subset.weight + 'kg';
+            const setReps = document.createElement('div');
+            setReps.textContent = subset.reps + ' reps';
+            const setRest = document.createElement('div');
+            setRest.textContent = formatRestTime(subset.restMinutes, subset.restSeconds);
+            setInformationDiv.appendChild(setWeight);
+            setInformationDiv.appendChild(setReps);
+            setInformationDiv.appendChild(setRest);
+            setToAppend.appendChild(setInformationDiv);
         })
+        const deleteButton = document.createElement('div');
+        deleteButton.className = 'delete-set';
+        deleteButton.textContent = 'x';
+        deleteButton.id = set.id;
+        deleteButton.addEventListener('click', deleteSetFromStorage);
+        deleteButton.name = name;
+        setToAppend.appendChild(deleteButton);
         console.log("Appending Set: " + setToAppend);
         pastSetsContainer.append(setToAppend);
     })
@@ -128,13 +169,12 @@ const resetState = () => {
 }
 
 const setCurrentExercise = (name) => {
+    exerciseForm.addEventListener('submit', createCurrentSet);
     console.log("Setting New Exercise...");
-    let currentSets = [];
-    console.log("Current sets:" + currentSets);
     exerciseName.textContent = name;
     exerciseList.style.display = 'none';
     addMenu.style.display = 'flex';
-
+    let setGroup = {}
     let exercise = localStorage.getItem(name);
     // Exercise will be null
     if (!exercise || JSON.parse(exercise).length === 0) {
@@ -148,17 +188,15 @@ const setCurrentExercise = (name) => {
     }
 
     // Adds a set
-    addButton.addEventListener('click', createCurrentSet);
-    addButton.currentArray = currentSets;
-    addButton.name = name;
+    exerciseForm.name = name;
+    exerciseForm.setGroup = setGroup;
 
     backButton.addEventListener('click', resetState);
 
     saveButton.addEventListener('click', saveSetToStorage);
     saveButton.name = name;
     saveButton.exercise = exercise;
-    console.log(exercise);
-    saveButton.currentSets = currentSets;
+    saveButton.setGroup = setGroup;
 }
 
 exercises.forEach(exercise => {
--- notes.md
+++ notes.md
@@ -1,6 +1,13 @@
-Features to implement:
-[ ] Editing Past Sets
-[ ] Charts to visualise progress
-[ ] Refactor element creation function
-[ ] Restructure localStorage data
-[X] Delete Past Sets
+Features to implement:\
+[ ] Editing Past Sets\
+[ ] Charts to visualise progress\
+[ ] Refactor element creation function\
+[ ] Duplicate set functionality\
+[ ] Delete current set\
+[ ] Home page overview to keep track of completed exercises\
+[X] Add minutes and seconds to rest input\
+[X] Restructure exercise data structure\
+[X] Delete Past Sets\
+
+Current bugs:
+- ~~Saving an empty set breaks delete functionality.~~
